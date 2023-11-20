05b8b79a62d43c017692d4a8d9643e362ca9c595
devkevin <rauppke@gmail.com>
2023-11-20 17:13:27 -0300

    Study JavaFX

--- src/main/java/test/layout/AppLayout.java
+++ src/main/java/test/layout/AppLayout.java
@@ -2,18 +2,13 @@ package test.layout;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        VBox temp = new VBox();
-        temp.getChildren().add(new Caixa().comTexto("1"));
-        temp.getChildren().add(new Quadrado());
-
-        Scene principal = new Scene(temp,800,600);
+        Scene principal = new Scene(new TesteAnchorPane(),800,600);
 
         stage.setScene(principal);
         stage.setTitle("Gerenciado de Layout");
