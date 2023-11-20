8c8a5dde3313024cc254fffd03790ad27f5ee56c
devkevin <rauppke@gmail.com>
2023-11-20 17:58:26 -0300

    Study JavaFX

--- src/main/java/test/layout/AppLayout.java
+++ src/main/java/test/layout/AppLayout.java
@@ -9,9 +9,10 @@ public class AppLayout extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent raiz = null;
-//        raiz = new TesteAnchorPane();
-//        raiz = new TesteBorderPane();
+        raiz = new TesteAnchorPane();
+        raiz = new TesteBorderPane();
         raiz = new TesteFlowPane();
+        raiz = new TesteGridPane();
 
         Scene principal = new Scene(raiz,800,600);
 
